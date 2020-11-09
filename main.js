4bb31db029e9dc01543f250382989b628d9b5abd
unknown <githublmao@seznam.cz>
2020-11-09 18:34:29 +0100

    zkousim js, pomoc

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let osoba = {
-    firstname: "",
     sex: "",
     age: "",
     weight: 0,
     height: 0,
 };
 
-function kalorie(){
-    
+console.log(osoba);
 
+function muz(w = this.weight, h = this.height, a = this.age) {
+    return (13.97*w+4.799*h-5.677*a+88.362); 
 }
+function zena(w = this.weight, h = this.height, a = this.age){
+    return(9.247*w+3.098*h-4.33*a+447.593)
+}
+console.log();
