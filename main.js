9978cd06803909002e8aa7dd2387416c365a8119
unknown <githublmao@seznam.cz>
2020-11-11 15:31:01 +0100

    Zacinam byt zoufaly, js me zabije

--- main.js
+++ main.js
@@ -3,22 +3,25 @@ let osoba = {
     sex: "",
     height: 0,
     weight: 0,
-};
 
-console.log(osoba);
-
-function muz(weight, height, age) {
-    return (13.97*weight+4.799*height-5.677*age+88.362); 
-};
-function zena(weight, height, age){
+muz: function(w = this.weight, h = this.height, a = this.age) {
+    newFunction();
+    return ((13.97*weight)+(4.799*height)-(5.677*age)+88.362); 
+},
+zena: function(w = this.weight, h = this.height, a = this.age){
     return(9.247*weight+3.098*height-4.33*age+447.593)
+},
 };
 document.getElementById('calc').addEventListener('click', function() {
-    osoba.age = document.getElementById('age').value;
     osoba.sex = document.getElementById('woman').checked ? 'Žena' : 'Muž';
+    osoba.age = document.getElementById('age').value;
     osoba.height = document.getElementById('height').value;
     osoba.weight = document.getElementById('weight').value;
-    document.getElementById('result').innerHTML = `kcal = ${muz()}`;        
+    document.getElementById('result').innerHTML = `kcal = ${osoba.muz()}`;        
 });
 
 console.log(weight);
+
+function newFunction() {
+    console.log((13.97 * 87) + (4.799 * 190) - (5.677 * 16) + 88.362);
+}
