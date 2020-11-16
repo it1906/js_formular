3bf44c78e6ee2828347289a4a70421eebc9df64f
unknown <githublmao@seznam.cz>
2020-11-16 22:27:46 +0100

    asi to funguje...jk jen napul

--- main.js
+++ main.js
@@ -4,24 +4,17 @@ let osoba = {
     height: 0,
     weight: 0,
 
-muz: function(w = this.weight, h = this.height, a = this.age) {
-    newFunction();
-    return ((13.97*w)+(4.799*h)-(5.677*a)+88.362).toFixed(0); 
-},
-zena: function(w = this.weight, h = this.height, a = this.age){
-    return(9.247*w+3.098*h-4.33*a+447.593).toFixed(0);
-},
+    muz: function (w = this.weight, h = this.height, a = this.age) {
+        return ((13.97 * w) + (4.799 * h) - (5.677 * a) + 88.362).toFixed(0);
+    },
+    zena: function (w = this.weight, h = this.height, a = this.age) {
+        return (9.247 * w + 3.098 * h - 4.33 * a + 447.593).toFixed(0);
+    },
 };
-document.getElementById('calc').addEventListener('click', function() {
+document.getElementById('calc').addEventListener('click', function () {
     osoba.sex = document.getElementById('woman').checked ? 'Žena' : 'Muž';
     osoba.age = document.getElementById('age').value;
     osoba.height = document.getElementById('height').value;
     osoba.weight = document.getElementById('weight').value;
-    document.getElementById('result').innerHTML = `Váš denní příjem kalorií pro udržení váhy je ${osoba.muz()} kcal`;        
+    document.getElementById('result').innerHTML = `Váš denní příjem kalorií pro udržení váhy je ${osoba.muz()} kcal. ${osoba.sex}`;
 });
-
-console.log(weight);
-
-function newFunction() {
-    console.log((13.97 * 87) + (4.799 * 190) - (5.677 * 16) + 88.362);
-}
